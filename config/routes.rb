f53f2ae9b2564be554fad828c801a8f9a0b7b34c
yuanchang <759337828@qq.com>
2019-10-28 13:27:01 +0000

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
       post :join
     end
   end
+
+  namespace :account do
+    resources :groups
+  end
+
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
